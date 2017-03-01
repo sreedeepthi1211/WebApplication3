3d711f5dd9f725f3c1594d9c2e0291a4ee9bce87
chigu <chigu@LAPTOP-BIQ60JPL>
2017-03-01 13:47:20 -0500

    Add project files.

---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Document pdfDoc = new Document(PageSize.A4, 25, 10, 25, 10);
+                PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+                pdfDoc.Open();
+                Paragraph Text = new Paragraph("This is test file");
+                pdfDoc.Add(Text);
+                pdfWriter.CloseStream = false;
+                pdfDoc.Close();
+                Response.Buffer = true;
+                Response.ContentType = "application/pdf";
+                Response.AddHeader("content-disposition", "attachment;filename=Example.pdf");
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.Write(pdfDoc);
+                Response.End();
+            }
+            catch (Exception ex)
+            { Response.Write(ex.Message); }
+        }
+    }
+}
